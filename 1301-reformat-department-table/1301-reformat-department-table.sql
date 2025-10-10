9a8007fb1f2b989f47fec4368e73d64f82a74dd7
Subham Panda <95236327+subhampanda30@users.noreply.github.com>
2025-10-10 23:31:07 +0530

    Time: 487 ms (74.05%), Space: 0B (100%) - LeetHub

---
+++ 1301-reformat-department-table/1301-reformat-department-table.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select Department.id,
+sum(if(month='jan',revenue,null)) as Jan_Revenue,
+sum(if(month='feb',revenue,null)) as Feb_Revenue,
+sum(if(month='mar',revenue,null)) as Mar_Revenue,
+sum(if(month='apr',revenue,null)) as Apr_Revenue,
+sum(if(month='may',revenue,null)) as May_Revenue,
+sum(if(month='jun',revenue,null)) as Jun_Revenue,
+sum(if(month='jul',revenue,null)) as Jul_Revenue,
+sum(if(month='aug',revenue,null)) as Aug_Revenue,
+sum(if(month='sep',revenue,null)) as Sep_Revenue,
+sum(if(month='oct',revenue,null)) as Oct_Revenue,
+sum(if(month='nov',revenue,null)) as Nov_Revenue,
+sum(if(month='dec',revenue,null)) as Dec_Revenue
+from Department
+group by id;
